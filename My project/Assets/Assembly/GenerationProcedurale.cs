0741435b9261691237f5913c017649d4bac9947e
Djolalo <djilali.bahous@outlook.com>
2023-02-15 15:05:55 +0100

    trying to debug things, quite weird

--- My project/Assets/Assembly/GenerationProcedurale.cs
+++ My project/Assets/Assembly/GenerationProcedurale.cs
@@ -48,9 +48,9 @@ public class GenerationProcedurale : MonoBehaviour
     {
         groundTilemap.ClearAllTiles();
         this.map = GenerateArray(this.width, this.height, true);
-        this.map = TerrainGeneration(this.map);
-        this.map = ajoutPierres(this.map);
-        this.map = generateTree(map, 25, 3, 1, 0.7F, 3);
+        TerrainGeneration();
+        ajoutPierres();
+        generateTree(90, 3, 25, 0.9F, 4);
         RenderMap(map, groundTilemap, caveTilemap, rockTile, groundTile, caveTile, skyTile, treeTile, leaveTile);
     }
 
@@ -67,7 +67,7 @@ public class GenerationProcedurale : MonoBehaviour
         return map;
     }
 
-    public textureTypes[,] TerrainGeneration(textureTypes[,] map)
+    public void TerrainGeneration()
     {
         int perlinHeight;
         for (int x = 0; x < width; x++)
@@ -77,12 +77,11 @@ public class GenerationProcedurale : MonoBehaviour
             for (int y = 0; y < perlinHeight; y++)
             {
                 float caveValue = Mathf.PerlinNoise((x * modifier) + seed, (y * modifier) + seed);
-                map[x, y] = (caveValue > 0.60) ? textureTypes.CAVERNE : textureTypes.TERRE;
+                this.map[x, y] = (caveValue > 0.60) ? textureTypes.CAVERNE : textureTypes.TERRE;
             }
         }
-        return map;
     }
-    public textureTypes[,] ajoutPierres(textureTypes[,] map)
+    public void ajoutPierres()
     {
         int perlinHeight;
         for (int x = 0; x < width; x++)
@@ -96,17 +95,15 @@ public class GenerationProcedurale : MonoBehaviour
                 float coefAvenir = (Mathf.PerlinNoise((x * modifier + seed) * 0.75F, (y * modifier + seed) * 0.75F));
                 if (map[x, y] == textureTypes.TERRE)
                 {
-                    map[x, y] = (underGroundTH > y) ? (coefAvenir > 0.3) ? textureTypes.PIERRE : textureTypes.TERRE : (coefAvenir < 0.3) ? textureTypes.PIERRE : map[x, y] = textureTypes.TERRE;
+                    map[x, y] = (underGroundTH > y) ? (coefAvenir > 0.3) ? textureTypes.PIERRE : textureTypes.TERRE : (coefAvenir < 0.3) ? textureTypes.PIERRE : textureTypes.TERRE;
                 }
             }
         }
-        return map;
     }
 
-    public textureTypes[,] generateTree(textureTypes[,] map, int angle, int largeur, int longueur, float reduction, int nbRecursion)
+    public void generateTree(int angle, int largeur, int longueur, float reduction, int nbRecursion)
     {
         float newAngle = (float)angle * 0.001745F;
-
         //tree spawns in the middle of the map
         int milieu = this.width / 2;
 
@@ -136,85 +133,146 @@ public class GenerationProcedurale : MonoBehaviour
 
         //nbRecursion == 0 is the end of recursivity condition
         nbRecursion--;
-        float oppositeAngle = (1.57f - (1.57f - newAngle));
-        map = leftTreeRecursion(map, oppositeAngle,largeur,longueur, reduction, nbRecursion , milieu, y);
-        map = rightTreeRecursion(map, newAngle, largeur, longueur , reduction, nbRecursion, milieu, y);
-        return map;
+        leftTreeRecursion(1.57f + newAngle,largeur,longueur, reduction, nbRecursion , milieu, y, newAngle);
+        rightTreeRecursion(1.57f - newAngle, largeur, longueur, reduction, nbRecursion, milieu, y, newAngle);
     }
-    public textureTypes[,] leftTreeRecursion(textureTypes[,] map, float angle, int largeur, int longueur, float reductionValue, int nbRecursion, int startingX, int startingY)
+    public void leftTreeRecursion(float currentAngle, int largeur, int longueur, float reductionValue, int nbRecursion, int startingX, int startingY, float rotationAngle)
     {
         UnityEngine.Debug.Log("<color=green> On part à gauche! </color>");
 
         //put leaves at the end of branches
         if (nbRecursion == 0)
         {
+            map[startingX, startingY] = textureTypes.BOIS;
             map[startingX, startingY + 1] = textureTypes.FEUILLE;
             map[startingX + 1, startingY + 1] = textureTypes.FEUILLE;
             map[startingX + 1, startingY] = textureTypes.FEUILLE;
             map[startingX - 1, startingY + 1] = textureTypes.FEUILLE;
             map[startingX - 1, startingY] = textureTypes.FEUILLE;
-            return map;
+            return;
         }
-
-
-        int pasX;
-        int startY = startingY;
-        int pasY= (int)(startingY * Mathf.Sin(angle));
-        startingY += pasY;
-        UnityEngine.Debug.Log(" X : " + startingX + "Y = " + startingY + " - " + pasY + " point de départ: "+startY);
-        while (startY <startingY)
+        if (nbRecursion >= 1)
         {
-            for (int z = 0; z < largeur; z++)
-            {
-                pasX = (int)(startingX * Mathf.Cos(angle));
-                startingX += (startingX - pasX);
-                map[startingX, startY] = textureTypes.BOIS;
-            }
-            UnityEngine.Debug.Log("X = " + startingX + " et y = " + startingY);
-            startY++;
+            float sX, sY; int x, y;
+            float finalX = (float)longueur * Mathf.Sin(currentAngle) + (float)startingX;
+            float finalY = (float)longueur * Mathf.Cos(currentAngle) + (float)startingY;
+            sX = (float)startingX;
+            sY = (float)startingY;
+            UnityEngine.Debug.Log("Coordonnées de départ : x" + sX + ", y " + sY + " Coordonnées d'arrivée : x " + finalX + ", y " + finalY);
+
+            bresenhamGeneral(sX,finalX,sY,finalY);
+            largeur = (int)(largeur * reductionValue);
+            longueur = (int)(longueur * reductionValue);
+            nbRecursion--;
+            x = Mathf.FloorToInt(finalX);
+            y = Mathf.FloorToInt(finalY);
+            leftTreeRecursion(currentAngle + rotationAngle, largeur, longueur, reductionValue, nbRecursion, x, y, rotationAngle);
+            rightTreeRecursion(currentAngle - rotationAngle, largeur, longueur, reductionValue, nbRecursion, x, y, rotationAngle);
         }
-        largeur = (int)(largeur * reductionValue);
-        longueur = (int)(longueur * reductionValue);
-        nbRecursion--;
-        map = leftTreeRecursion(map, angle, largeur ,longueur , reductionValue, nbRecursion, startingX, startingY);
-        map = rightTreeRecursion(map, 1.57f-(1.57f-angle), largeur , longueur , reductionValue, nbRecursion , startingX, startingY);
-        return map;
     }
-    public textureTypes[,] rightTreeRecursion(textureTypes[,] map, float angle, int largeur, int longueur, float reductionValue, int nbRecursion, int startingX, int startingY)
+
+    public void rightTreeRecursion(float currentAngle, int largeur, int longueur, float reductionValue, int nbRecursion, int startingX, int startingY, float rotationAngle)
     {
         UnityEngine.Debug.Log("<color=green>On part à droite! </color>");
         if (nbRecursion == 0)
         {
+            map[startingX, startingY] = textureTypes.BOIS;
             map[startingX, startingY + 1] = textureTypes.FEUILLE;
             map[startingX + 1, startingY + 1] = textureTypes.FEUILLE;
             map[startingX + 1, startingY] = textureTypes.FEUILLE;
             map[startingX - 1, startingY + 1] = textureTypes.FEUILLE;
             map[startingX - 1, startingY] = textureTypes.FEUILLE;
-            return map;
+            return;
         }
-        int pasX; int startY = startingY;
-        int pasY = (int)(startingY * Mathf.Sin(angle));
-        startingY += pasY;
-        UnityEngine.Debug.Log(" X : " + startingX + "Y = " + startingY + " - " + pasY + " point de départ: " + startY);
-        while (startY < startingY)
+        if (nbRecursion >= 1)
         {
-            for (int z = 0; z < largeur; z++)
+            float sX, sY; int x, y;
+            float finalX = (float)longueur * Mathf.Sin(currentAngle) + (float)startingX;
+            float finalY = (float)longueur* Mathf.Cos(currentAngle) + (float)startingY;
+            sX = (float)startingX;
+            sY = (float)startingY;
+            UnityEngine.Debug.Log("Coordonnées de départ : x" + sX + ", y " + sY + " Coordonnées d'arrivée : x " + finalX + ", y " + finalY);
+
+            for (int i = 0; i < finalY - startingY; i++)
             {
-                pasX = (int)(startingX * Mathf.Cos(angle));
-                startingX += (startingX - pasX);
-                map[startingX, startY] = textureTypes.BOIS;
+                bresenhamGeneral(sX + i, finalX + i, sY, finalY);
             }
-            startY++;
-            UnityEngine.Debug.Log("X = " + startingX + " et y = " + startingY);
+            largeur = (int)(largeur * reductionValue);
+            longueur = (int)(longueur * reductionValue);
+            nbRecursion--;
+            x = Mathf.FloorToInt(finalX);
+            y = Mathf.FloorToInt(finalY);
+            leftTreeRecursion(currentAngle + rotationAngle, largeur, longueur, reductionValue, nbRecursion, x, y, rotationAngle);
+            rightTreeRecursion(currentAngle - rotationAngle, largeur, longueur, reductionValue, nbRecursion, x, y, rotationAngle);
         }
-        largeur = (int)(largeur * reductionValue);
-        longueur = (int)(longueur * reductionValue);
-        nbRecursion--;
-        map = leftTreeRecursion(map, 1.57f - (1.57f - angle), largeur, longueur, reductionValue, nbRecursion , startingX, startingY);
-        map = rightTreeRecursion(map, -angle, largeur, longueur, reductionValue, nbRecursion , startingX, startingY);
-        return map;
+    }
+    public void bresenham(float x1, float x2, float y1, float y2, float IncrX, float IncrY, float dx, float dy, bool inversion)
+    {
+        float IncreE = 2 * dy;
+        float IncreNE = 2 * (dy - dx);
+        float dp = 2 * (dy - dx);
+        float y = y1;
+        int abcisse, ordonnee;
+
+        for (float x = x1; x != x2; x += IncrX)
+        {
+            abcisse = (int)(x);
+            ordonnee = (int)(y);
+            if (inversion) map[abcisse, ordonnee] = textureTypes.BOIS;
+            else map[ordonnee, abcisse]= textureTypes.BOIS;
+            if (dp <= 0)
+            {
+                dp += IncreE;
+            }
+            else
+            {
+                y += IncrY;
+                dp += IncreNE;
+            }
+        }
     }
 
+
+    public void bresenhamGeneral(float x1, float x2, float y1, float y2)
+    {
+        /*
+        We only keep the integer part of the coordinates to avoid in loop issue.
+        */
+        x1 = Mathf.Floor(x1);
+        x2 = Mathf.Floor(x2);
+        y1 = Mathf.Floor(y1);
+        y2 = Mathf.Floor(y2);
+        float dy = y2 - y1;
+        float dx = x2 - x1;
+        float Incry, Incrx;
+        if (dx > 0)
+        {
+            Incrx = 1f;
+        }
+        else
+        {
+            Incrx = -1f;
+            dx = -dx;
+        }
+        if (dy > 0)
+        {
+            Incry = 1f;
+        }
+        else
+        {
+            dy = -dy;
+            Incry = -1f;
+        }
+        if (dx >= dy)
+        {
+            bresenham(x1, x2, y1, y2, Incrx, Incry, dx, dy, false);
+        }
+        else
+        {
+
+            bresenham(y1, y2, x1, x2, Incry, Incrx, dy, dx, true);
+        }
+    }
     public void RenderMap(textureTypes[,] map, Tilemap groundTileMap, Tilemap caveTilemap, TileBase rockTilebase, TileBase groundTilebase, TileBase caveTilebase, TileBase skyTilebase, TileBase woodTile, TileBase leaveTile)
     {
         for (int x = 0; x < width; x++)
