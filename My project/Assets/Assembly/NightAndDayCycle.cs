9e4cfd30e3049ad2b931d7cfe4f7ec6d318a80e2
alicemabille <alicemabille@yahoo.fr>
2023-02-19 21:30:06 +0100

    Created a shader and material with it, 2d lights now effective
    really bad basic URP 2D sprite shader

---
+++ My project/Assets/Assembly/NightAndDayCycle.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NightAndDayCycle : MonoBehaviour
+{
+    private const int maxTime = 500;
+
+    [SerializeField] private Gradient lightColor;
+    [SerializeField] private GameObject light;
+
+    private int days;
+    private float time = 50;
+    private bool canChangeDay = true;
+    
+
+    public delegate void OnDayChanged();
+
+    public OnDayChanged DayChanged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(time > maxTime)
+        {
+            time = 0;
+        }
+
+        if( (int) time == maxTime/2 && canChangeDay)
+        {
+            canChangeDay = false;
+            if(DayChanged!= null) { DayChanged(); }
+            days++;
+        }
+
+        if( (int) time == maxTime/2 + 2) canChangeDay = true;
+
+        time += Time.deltaTime;
+
+        /*evaluate method should get a value between 0 and 1
+         *so we take the inverse of maxTime
+         * */
+        light.GetComponent<UnityEngine.Rendering.Universal.Light2D>().color = lightColor.Evaluate(time / maxTime);
+    }
+
+    public int getDays()
+    {
+        return days;
+    }
+}
